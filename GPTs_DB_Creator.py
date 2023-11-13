e04932ecadb9977a35c5222ee6ca2a5ea93f5989
JH Baek <zirubak@gmail.com>
2023-11-12 16:10:23 -0800

    Add init codes, README and Apache 2.0 License
    - Created a README.md and init files to provide an overview of the project, including installation, usage, and features.
    - Added an Apache 2.0 License to the project to clearly define the terms and conditions for use, reproduction, and distribution.
    - The README includes concise instructions for setting up a Conda environment and running the application, as well as a description of the file formats supported.
    - The Apache 2.0 License ensures open-source compliance and broad usage potential for the project.

---
+++ GPTs_DB_Creator.py
@@ -0,0 +1,96 @@
+# Import necessary libraries
+import streamlit as st
+import os
+import datetime
+from PyPDF2 import PdfReader
+import docx
+import textract
+import pandas as pd
+import time
+
+# Function to extract text from different file types
+def extract_text(file, file_type):
+    # If the file is a PDF
+    if file_type == 'pdf':
+        # Create a PDF reader object
+        reader = PdfReader(file)
+        # Extract text from each page and join them together
+        text = '\n'.join([page.extract_text() for page in reader.pages if page.extract_text()])
+    # If the file is a Word document
+    elif file_type == 'docx':
+        # Create a Document object
+        doc = docx.Document(file)
+        # Extract text from each paragraph and join them together
+        text = '\n'.join([para.text for para in doc.paragraphs])
+    # If the file is a text file
+    elif file_type == 'txt':
+        # Read the file and decode it to UTF-8
+        text = file.read().decode('utf-8')
+    # If the file is an Excel file
+    elif file_type in ['xlsx', 'xls']:
+        # Read the Excel file into a pandas DataFrame
+        df = pd.read_excel(file)
+        # Convert all cells to string and concatenate with newline characters
+        text = '\n'.join(df.astype(str).stack().tolist())
+    # If the file is a CSV file
+    elif file_type == 'csv':
+        # Read the CSV file into a pandas DataFrame
+        df = pd.read_csv(file)
+        # Convert all cells to string and concatenate with newline characters
+        text = '\n'.join(df.astype(str).stack().tolist())
+    # If the file type is not supported
+    else:
+        # Return an empty string
+        text = ''
+    return text
+
+# Function to append text to a file
+def append_to_file(text, file_path):
+    # Open the file in append mode
+    with open(file_path, 'a', encoding='utf-8', errors='replace') as file:
+        # add time sleep two seconds
+        time.sleep(2)
+        # Add a timestamp to the appended text
+        file.write(f"\n\n\n### Appended your data: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+        # Write the text to the file
+        file.write("\n\n" + text + "\n\n")
+
+# Streamlit UI
+# Use HTML to center align the title with bright green color
+st.markdown("""
+    <h1 style='text-align: center; color: limegreen;'>💻 Auto GPTs DB Text Creator 💾</h1>
+    """, unsafe_allow_html=True)
+# Adding a description
+st.markdown("""
+##### This application allows you to upload PDF, Word, or text files and appends their content to a text file in a database folder. \
+The appended data includes a timestamp and the text extracted from the uploaded file to use GPTs to manage your data as one text file to aovid potensial error in GPTs. 
+The database text files are named in the format `gpts_db_<current_date>.txt`. 
+This tool is useful for aggregating and storing text data from various documents in a structured format.
+""")
+
+# Create a file uploader widget
+uploaded_file = st.file_uploader("Upload a file", type=['pdf', 'docx', 'txt', 'xlsx', 'xls', 'csv'])
+
+# If a file is uploaded
+if uploaded_file is not None:
+    # Check if the 'db' folder exists, if not, create it
+    if not os.path.exists('db'):
+        os.mkdir('db')
+
+    # Create a file name with the current date
+    file_name = f"gpts_db_{datetime.datetime.now().strftime('%Y-%m-%d')}.txt"
+    # Create a file path
+    file_path = os.path.join('db', file_name)
+
+    # Show a spinner while the file is being processed
+    with st.spinner('Processing file...'):
+        # Extract the file type from the file name
+        file_type = uploaded_file.name.split('.')[-1]
+        # Extract text from the uploaded file
+        extracted_text = extract_text(uploaded_file, file_type)
+
+        # Append the extracted text to the file
+        append_to_file(extracted_text, file_path)
+
+    # Show a success message when the file is processed
+    st.success("File processed and text appended successfully.")
--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# GPTs_DB_Creator
-Create text DB for GPTs service
+# Auto GPTs DB Text Creator
+
+The current GPTs configration has issue to use mutiple file and performance is not good. This is a simple tool to combine all the text files into one file. You can use this tool to create a text file for GPTs DB and upload to GPTs Configuration.
+This Streamlit-based application automates appending text data from various file formats (PDF, Word, Text, Excel, CSV) into a single text file. It's designed for aggregating and storing text data, facilitating easier data management with GPTs.
+
+## Prerequisites
+
+- Python 3
+- Conda (for environment management)
+
+## Installation
+
+1. Clone or download the repository.
+2. Use Conda to create an environment from the provided `environment.yml` file:
+
+   ```bash
+   conda env create --name my-env-name --file environment.yml
+   ```
+
+3. Activate the new environment:
+
+    ```bash
+    conda activate my-env-name
+    ```
+4. Navigate to the directory containing the application files.
+
+## Usage
+Run the application with Streamlit:
+``` base
+streamlit run [script_name].py
+```
+
+Replace [script_name] with the name of the Python script.
+
+## Features
+- File Upload: Supports PDF, DOCX, TXT, XLSX, XLS, CSV.
+- Automatic Text Extraction: Extracts text from uploaded files.
+- Appending to Database File: Appends text to gpts_db_<current_date>.txt in db directory.
+- Timestamps: Each entry is timestamped.
+- Progress Indication: Visual feedback during file processing.
+
+
+## License
+This project is licensed under the Apache License 2.0 - see the [LICENSE](LICENSE) file for details.
+
